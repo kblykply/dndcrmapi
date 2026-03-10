73ac71ac3dcadf587a3e362aaec610b3b0d9afaf
kblykply <kubilaykuplay@gmail.com>
2026-03-10 18:25:35 +0300

    Add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,28 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
+
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { LeadsModule } from "./leads/leads.module";
-import { join } from "path";
 import { TasksModule } from "./tasks/tasks.module";
 import { MetaModule } from "./integrations/meta/meta.module";
-
-
-
-
+import { HealthController } from "./health.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-  envFilePath: join(process.cwd(), ".env"),
+      envFilePath: join(process.cwd(), ".env"),
     }),
     PrismaModule,
     AuthModule,
     UsersModule,
     LeadsModule,
-        TasksModule,
-            MetaModule,
-
-
+    TasksModule,
+    MetaModule,
   ],
+  controllers: [HealthController],
 })
 export class AppModule {}
